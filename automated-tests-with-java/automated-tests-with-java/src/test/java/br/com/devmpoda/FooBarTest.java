d622d76dec1b9b357d55fd4c32b735dc28be939a
Matheus Poda <matheus.poda@gmail.com>
2023-08-13 18:10:18 -0300

    Adiciona primeiro projeto de estudos de teste unitário com Java

---
+++ automated-tests-with-java/automated-tests-with-java/src/test/java/br/com/devmpoda/FooBarTest.java
@@ -0,0 +1,13 @@
+package br.com.devmpoda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FooBarTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+}
